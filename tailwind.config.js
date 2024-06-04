543ca64249820bee8fe4de76d46da42de053513a
akobir9990 <akobir.qoshshayev@gmail.com>
2024-06-04 09:15:12 +0500

    pefr: deleted comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   mode: "jit",
-  // These paths are just examples, customize them to match your project structure
   content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
-  theme: {
-    // ...
-  },
-  // ...
+  theme: {},
 };
