ab74a58ba8eb77b96c09dff35610d4ef04e2cc60
akobir9990 <akobir.qoshshayev@gmail.com>
2024-06-04 09:26:08 +0500

    refactor: changed science name

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -6,9 +6,9 @@ function index() {
   return (
     <div className="h-[100vh]">
       <Card className=" w-[100%] flex justify-ceenter items-center flex-wrap px-[50px] py-[70px]">
-        <div className="flex justify-center items-center flex-col text-3xl font-bold p-3 w-[150px] h-[150px] ">
+        <div className="flex justify-center items-center text-center flex-col text-3xl font-bold p-3 w-[150px] h-[150px] ">
           <img className="" src={logo} alt="???" />
-          ATJMM
+          Amaliy ekonometrika
         </div>
         <p className="p-3 min-w-[200px] w-[700px]">
           Ushbu dastur axborot texnologiyalari va jarayonlarni matematik
