471ce4651c5d218b3f2efd1a12e6bd7079d6b11e
akobir9990 <akobir.qoshshayev@gmail.com>
2024-06-04 09:28:55 +0500

    perf: chaned presentation's objects themeName's value

--- src/pages/presentation/index.jsx
+++ src/pages/presentation/index.jsx
@@ -15,96 +15,96 @@ export default function BasicCard() {
       id: 1,
       path: "",
       url: "1",
-      themeName:
-        '"Axborot texnologiyalari va jarayonlarni matematik modellashtirish" fanning asosiy tushunchalari. Ma`lumotlarni kodlash. Axborot jarayonlarining texnik va dasturiy ta`minoti.',
+      themeName: "Ekonometrik modellashtirish asoslari",
     },
     {
       id: 2,
       path: "",
       url: "2",
-      themeName:
-        "Mutaxassislik masalalarini yech-ishda axborot tenologiyalarini qo'llash. MS Office dasturlari.",
+      themeName: "Matematik statistika elementlari",
     },
     {
       id: 3,
       path: "",
       url: "3",
-      themeName: "Kompyuter tarmoglari va tarmoq texnologiyalari.",
+      themeName:
+        "Bir o'zgaruvchili chiziqli regressiya va korrelyatsiya modellari",
     },
     {
       id: 4,
       path: "",
       url: "4",
-      themeName: "Algoritmning xossalari.Algoritmni tasvirlash usullari.",
+      themeName:
+        "Bir o'zgaruvchili chiziqli regressiya va korrelyatsiya modellari",
     },
     {
       id: 5,
       path: "",
       url: "5",
-      themeName:
-        "Dasturlash tili elementlari. Masalalarini chiziqli algoritmlar yordamida dasturlash.",
+      themeName: "Regressiya koeffitsientlarining aniqligi va siljimasligi",
     },
     {
       id: 6,
       path: "",
       url: "6",
-      themeName:
-        "Masalalarni tarmoqlanuvchi al-goritmlar yordamida dasturlash.",
+      themeName: "Regressiya koeffitsientlarining aniqligi va siljimasligi",
     },
     {
       id: 7,
       path: "",
       url: "7",
-      themeName:
-        "Masalalarni tarmoqlanuvchi al-goritmlar yordamida dasturlash.",
+      themeName: "Ko'p o'zgaruvchili regressiya tahlili",
     },
     {
       id: 8,
       path: "",
       url: "8",
-      themeName: "Masalalarini dasturlashda massivlardan foydalanish",
+      themeName:
+        "Ko'p omilli regressiya koeffitsientlarining aniqligi va siljimasligi",
     },
     {
       id: 9,
       path: "",
       url: "9",
-      themeName: "Masalalarni dasturlashda funktsiyalardan foydalanish.",
+      themeName:
+        "Kop omilli korrelyasiya - regressiya modeli uchun omillarni tanlash",
     },
     {
       id: 10,
       path: "",
       url: "10",
-      themeName: "MathCAD dasturining interfeysi. Funktsiyalar. Grafika.",
+      themeName:
+        "Ishlab chigarish funksiyalari orgali ekonometrik modellashtirish",
     },
     {
       id: 11,
       path: "",
       url: "11",
-      themeName: "MathCad dasturida algoritmlash va dasturlash.",
+      themeName: `Chiziqli bolmagan modellarda ko'p o'zgaruvchili regressiya`,
     },
     {
       id: 12,
       path: "",
       url: "12",
-      themeName: "Matematik model va modelashtirish.",
+      themeName: "Ekonometrik tenglamalar sistemasi unt echish usullari",
     },
     {
       id: 13,
       path: "",
       url: "13",
-      themeName: "Chizigli modellar va ularni yechish.",
+      themeName: "Vaqtli qatorlar va ularning tenglamalarini tuzish usullari",
     },
     {
       id: 14,
       path: "",
       url: "14",
-      themeName: "Chizigsiz modellar va ularni yechish.",
+      themeName: "Trend va uning asosiy turlari",
     },
     {
       id: 15,
       path: "",
       url: "15",
-      themeName: "Chizigli dasturlash masalasini grafik lusulida yechish",
+      themeName: "Chizigli ekonometrik model orgali prognoz gilish.",
     },
   ];
   return (
